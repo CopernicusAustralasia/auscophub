c3b6aea865dfb8e7f4b4aa69e9d8110e6ce84a11
Neil Flood <Neil.Flood@derm.qld.gov.au>
2016-06-30 14:35:24 +1000

    Added auscophubmeta.py

---
+++ auscophub/auscophubmeta.py
@@ -0,0 +1,82 @@
+"""
+Class for reading the small XML files we generate to go with each SAFE format
+zipfile
+
+"""
+from __future__ import print_function, division
+
+import os
+import datetime
+from xml.dom import minidom
+
+class AusCopHubMeta(object):
+    """
+    Class for reading the small XML metadata files we use on the Aus 
+    Copernicus Hub server, to identify and chanracterise the SAFE format
+    zipfiles delivered by ESA. 
+    
+    Same class is used for Sentinel-1 and Sentinel-2 (and probably 3 when we get to it). Not
+    all attributes will be present, depending on the satellite. 
+    
+    Attributes:
+        satellite               String, e.g. S1A, S2A, etc. 
+        ctrLong                 Float, longitude of centroid of imagery
+        ctrLat                  Float, latitude of centroid of imagery
+        cloudCoverPcnt          Int, percentage cloud cover
+        acquisitiondatetime     datetime object, for acquisition time (in UTC)
+        footprintWkt            WKT string of rough footprint, as supplied by ESA
+        esaSoftwareVersion      String, ESA's processing software version number
+        esaProcessingTimeStr    String, time at which ESA processed (in UTC)
+        polarisationValuesList  List of strings, radar polarisation values (e.g. HV, VV, ...)
+        swathValuesList         List of strings, radar swath-type values (e.g. IW1, IW2,..)
+    
+    """
+    def __init__(self, filename):
+        "Create from a given filename"
+        if not os.access(filename, os.R_OK):
+            raise AusCopHubMetaError("XML file '{}' not found".format(filename))
+        
+        xmlStr = open(filename).read()
+        doc = minidom.parseString(xmlStr)
+        
+        safeDescrNodeList = doc.getElementsByTagName('AUSCOPHUB_SAFE_FILEDESCRIPTION')
+        if len(safeDescrNodeList) == 0:
+            raise AusCopHubMetaError("XML file '{}' is not AUSCOPHUB_SAFE_FILEDESCRIPTION file".format(filename))
+
+        safeDescrNode = safeDescrNodeList[0]
+        self.satellite = (safeDescrNode.getElementsByTagName('SATELLITE')[0]).getAttribute('name')
+        centroidNodeList = safeDescrNode.getElementsByTagName('CENTROID')
+        if len(centroidNodeList) > 0:
+            self.ctrLong = float(centroidNodeList[0].getAttribute('longitude'))
+            self.ctrLat = float(centroidNodeList[0].getAttribute('latitude'))
+        cloudCoverNodeList = safeDescrNode.getElementsByTagName('ESA_CLOUD_COVER')
+        if len(cloudCoverNodeList) > 0:
+            self.cloudCoverPcnt = int(cloudCoverNodeList[0].getAttribute('percentage'))
+        
+        acqTimeNodeList = safeDescrNode.getElementsByTagName('ACQUISITION_TIME')
+        if len(acqTimeNodeList) > 0:
+            acqTimeStr = acqTimeNodeList[0].getAttribute('datetime_utc')
+            self.acquisitiondatetime = datetime.datetime.strptime(acqTimeStr, '%Y-%m-%d %H:%M:%S')
+        
+        footprintNodeList = safeDescrNode.getElementsByTagName('ESA_TILEOUTLINE_FOOTPRINT_WKT')
+        if len(footprintNodeList) > 0:
+            self.footprintWkt = footprintNodeList[0].firstChild.data.strip()
+        
+        processingNodeList = safeDescrNode.getElementsByTagName('ESA_PROCESSING')
+        if len(processingNodeList) > 0:
+            self.esaSoftwareVersion = processingNodeList[0].getAttribute('software_version')
+            self.esaProcessingTimeStr = processingNodeList[0].getAttribute('processingtime_utc')
+        
+        polarisationNodeList = safeDescrNode.getElementsByTagName('POLARISATION')
+        if len(polarisationNodeList) > 0:
+            self.polarisationValuesList = polarisationNodeList[0].getAttribute('values').split(',')
+        
+        swathNodeList = safeDescrNode.getElementsByTagName('SWATH')
+        if len(swathNodeList) > 0:
+            self.swathValuesList = swathNodeList[0].getAttribute('values').split(',')
+        
+        
+
+
+
+class AusCopHubMetaError(Exception): pass
