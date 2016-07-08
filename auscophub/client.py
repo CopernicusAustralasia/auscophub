a17b8e011136457c47d115113473689c325c3cdc
Neil Flood <Neil.Flood@derm.qld.gov.au>
2016-07-08 12:35:51 +1000

    client.py: Re-organise class and classes, now that I can see what the THREDDS server will actually serve up.

--- auscophub/client.py
+++ auscophub/client.py
@@ -13,19 +13,21 @@ import sys
 from xml.dom import minidom
 import xml.parsers.expat
 
+from auscophub import auscophubmeta
+
+
 isPython3 = (sys.version_info.major == 3)
 if isPython3:
     from urllib.request import build_opener, ProxyHandler
 else:
     from urllib2 import build_opener, ProxyHandler
 
-# These URLs seems like they might not be constant, but it is not clear how
-# I would get to them from the top level, without an undue dependence on the
-# "pretty" formatting on the NCI front page. So, they are hard-wired for now.
-# They give the starting point into the XML catalog files for the Copernicus
-# data. 
-THREDDS_SEN1_BASE_URL = "http://dapds00.nci.org.au/thredds/catalog/fj7/SAR/Sentinel-1"
-THREDDS_SEN2_BASE_URL = "http://dapds00.nci.org.au/thredds/catalog/fj7/MSI/Sentinel-2"
+THREDDS_BASE = "http://dapds00.nci.org.au/thredds"
+THREDDS_CATALOG_BASE = "{}/catalog".format(THREDDS_BASE)
+THREDDS_FILES_BASE = "{}/fileServer".format(THREDDS_BASE)
+THREDDS_COPERNICUS_SUBDIR = "uc0/fj7_dev/Copernicus"
+THREDDS_SEN1_CATALOG_BASE = "{}/{}/Sentinel-1A/C-SAR".format(THREDDS_CATALOG_BASE, THREDDS_COPERNICUS_SUBDIR)
+THREDDS_SEN2_CATALOG_BASE = "{}/{}/Sentinel-2A/MSI".format(THREDDS_CATALOG_BASE, THREDDS_COPERNICUS_SUBDIR)
 
 
 def makeUrlOpener(proxy=None):
@@ -40,7 +42,25 @@ def makeUrlOpener(proxy=None):
         proxyHandler = ProxyHandler({'http':proxy})
         opener = build_opener(proxyHandler)
     return opener
+
+
+def loadDatasetDescriptionXmlList(urlOpener, xmlDatasetEntryList):
+    """
+    Given a list of DatasetEntry object, all of which correspond to .xml files
+    on the server, load the XML into AusCopHubMeta objects and return 
+    a list of these objects. 
     
+    """
+    metaObjList = []
+    for dsObj in xmlDatasetEntryList:
+        if not dsObj.name.endswith('.xml'):
+            raise AusCopHubClientError("DatasetEntry object '{}' does not end in '.xml'".format(dsObj.name))
+
+        xmlStr = urlOpener.open(dsObj.fullUrl).read()
+        metaObj = auscophubmeta.AusCopHubMeta(xmlStr=xmlStr)
+        metaObjList.append(metaObj)
+    return metaObjList
+
 
 class ThreddsServerDirList(object):
     """
@@ -49,50 +69,52 @@ class ThreddsServerDirList(object):
     
     Attributes:
         subdirs             list of subdirectories under this one, i.e. <catalogRef> tags
-        datasets            list of dtasets in this subdir, i.e. <dataset> tags
+        datasets            list of datasets in this subdir, i.e. <dataset> tags
         
     """
-    def __init__(self, urlOpener, baseUrl, subDir):
-        doc = getCatalogXml(urlOpener, baseUrl, subDir)
+    def __init__(self, urlOpener, subdirUrl):
+        doc = getThreddsCatalogXml(urlOpener, subdirUrl)        
         
         catalogNode = doc.getElementsByTagName('catalog')[0]
         topDatasetNode = catalogNode.getElementsByTagName('dataset')[0]
         
         subdirNodeList = topDatasetNode.getElementsByTagName('catalogRef')
-        self.subdirs = [CatalogRefEntry(node) for node in subdirNodeList]
+        self.subdirs = [ThreddsCatalogRefEntry(dirFullUrl, node) for node in subdirNodeList]
         
         datasetNodeList = topDatasetNode.getElementsByTagName('dataset')
-        self.datasets = [DatasetEntry(node) for node in datasetNodeList]
+        self.datasets = [ThreddsDatasetEntry(node) for node in datasetNodeList]
 
 
-class DatasetEntry(object):
+class ThreddsDatasetEntry(object):
     """
     Details of a <dataset> tag in the catalog.xml
     """
     def __init__(self, datasetNode):
         self.name = datasetNode.getAttribute('name').strip()
         self.urlPath = datasetNode.getAttribute('urlPath').strip()
+        self.fullUrl = "{}/{}".format(THREDDS_FILES_BASE, self.urlPath)
 
 
-class CatalogRefEntry(object):
+class ThreddsCatalogRefEntry(object):
     """
     Details of a <catalogRef> tag in the catalog.xml
     """
-    def __init__(self, catalogRefNode):
+    def __init__(self, baseUrl, catalogRefNode):
         self.href = catalogRefNode.getAttribute('xlink:href').strip()
         self.idStr = catalogRefNode.getAttribute('ID').strip()
+        self.fullUrl = "{}/{}".format(THREDDS_CATALOG_BASE, self.idStr)
 
 
-def getCatalogXml(urlOpener, baseUrl, subDir, returnXmlString=False):
+def getThreddsCatalogXml(urlOpener, baseUrl, returnXmlString=False):
     """
-    Get the catalog.xml file for the given subDir under the given baseUrl
+    Get the catalog.xml file for the given baseUrl
     By default, it will parse the catalog.xml using minidom and return the document object. 
     If the XML fails to parse, then the return value will be None.
     
     If returnXmlString is True, then just return the XML string, without parsing. 
     
     """
-    url = "{}/{}/catalog.xml".format(baseUrl, subDir)
+    url = "{}/catalog.xml".format(baseUrl)
     reader = urlOpener.open(url)
     xmlStr = reader.read()
     
@@ -106,3 +128,5 @@ def getCatalogXml(urlOpener, baseUrl, subDir, returnXmlString=False):
         
     return returnVal
 
+
+class AusCopHubClientError(Exception): pass
